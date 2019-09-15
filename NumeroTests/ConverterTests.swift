13f6bc628552b5b6a44b6decaba4bcaa7bba4b26
wei-tsung-cheng <william.cheng@kkday.com>
2019-09-15 22:54:58 +0800

    WW

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -47,7 +47,7 @@ class ConverterTests: XCTestCase {
     
     func testConversionForOne() {
         let result = converter.convert(2)
-        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+        XCTAssertEqual(result, "I", "Conversion for 2 is incorrect")
     }
     
     func testAdd() {
